f57960b75ae7c5407c8bcae60aea532d4886284f
戴江枫 <aaron.dai@dkongjian.cn>
2017-12-19 21:20:27 +0800

    update singleton with synchronized

--- src/main/java/org/alens/study/designpattern/singleton/Singleton.java
+++ src/main/java/org/alens/study/designpattern/singleton/Singleton.java
@@ -10,8 +10,15 @@ public class Singleton {
 
     public static Singleton getSingleton() {
         if (singleton == null) {
-            singleton = new Singleton();
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
         }
+
         return singleton;
     }
 }
+
+
